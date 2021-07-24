ec8b4d70b9f1fa57d3f58c5d22f86928d770a84c
ihsun <ihsun@ihsuns-MacBook-Air.local>
2021-07-25 03:26:08 +0430

    better syntax for form

--- accounts/forms.py
+++ accounts/forms.py
@@ -9,7 +9,13 @@ class SignupForm(UserCreationForm):
 
 	class Meta:
 		model = User
-		fields = ('username', 'email', 'password1', 'password2', 'image')
+		fields = (
+			'username',
+			'email',
+			'password1',
+			'password2',
+			'image',
+		)
 
 
 class LoginForm(AuthenticationForm):
