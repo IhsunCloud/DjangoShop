1261ade6da407cb07e8fa45280f1c43674193ced
ihsun <ihsun@ihsuns-MacBook-Air.local>
2021-07-25 04:13:40 +0430

    add checkout view

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+
+class CheckoutView(generic.View):
+    template_name = 'pages/shop/checkout.html'
