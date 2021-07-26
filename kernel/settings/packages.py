1ebb84dea220d50f2790553cdf45480f740e3596
ihsun <ihsun@ihsuns-MacBook-Air.local>
2021-07-26 04:38:59 +0430

    add an package

--- kernel/settings/packages.py
+++ kernel/settings/packages.py
@@ -9,6 +9,7 @@ INSTALLED_APPS.append('mptt'),
 INSTALLED_APPS.append('sorl.thumbnail'),
 INSTALLED_APPS.append('taggit'),
 
+
 # My Apps -->
 INSTALLED_APPS.append('accounts.apps.AccountsConfig'),
 INSTALLED_APPS.append('blog.apps.BlogConfig'),
@@ -18,7 +19,9 @@ INSTALLED_APPS.append('painless'),
 INSTALLED_APPS.append('payment.apps.PaymentConfig'),
 INSTALLED_APPS.append('shop.apps.ShopConfig'),
 
+
 AUTH_USER_MODEL = "accounts.User"
+
 
 ALLOW_UNICODE_SLUGS = True
 
