67dab36a1c2527124bf3ad595f33bbfd9e2e405e
ihsunmozdeh <ihsun@tuta.io>
2022-12-11 17:09:13 +0330

    Add phone number.

--- accounts/models.py
+++ accounts/models.py
@@ -6,4 +6,6 @@ class User(AbstractUser):
 	""" My Customized User Model. """
 	
 	username = models.CharField(max_length=64,unique=True, null=True, blank=True)
-	image = models.ImageField(upload_to='profiles/', null=True, blank=True)
+	avatar = models.ImageField(upload_to='profiles/', null=True, blank=True)
+	phone = models.CharField(max_length=16, unique=True)
+	
