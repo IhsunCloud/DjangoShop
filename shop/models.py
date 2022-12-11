025706e7415adfb3d56ec9158b40d8656f7fc8dd
ihsunmozdeh <ihsun@tuta.io>
2022-12-11 17:22:15 +0330

    🚀 Add rating.

--- shop/models.py
+++ shop/models.py
@@ -3,3 +3,5 @@ from .submodels.color import Color
 from .submodels.comment import Comment
 from .submodels.image import Image
 from .submodels.product import Product
+from .submodels.rating import RatedItem
+from .submodels.rating import Rating
